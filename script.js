95be3c897262d77c11b4a22f47236fd33cc20ac6
Tomnadoo <Tomnadooadyorough@gmail.com>
2025-05-10 09:22:42 +0100

    initial changes

--- script.js
+++ script.js
@@ -5,61 +5,53 @@ const students = [
       lastName: "Jude",
       age: 19,
       score: 85,
-      courses: ["Quantum Computing", "AI-Driven Mathematics"],
-      hobbies: "Basketball, Coding AI models"
+      courses: ["Quantum Computing", "AI-Driven Mathematics"]
     },
     {
       firstName: "Adebayo",
       lastName: "Adekunle",
       age: 21,
       score: 90,
-      courses: ["Synthetic Biology", "CRISPR Technology"],
-      hobbies: "Music, Biohacking"
+      courses: ["Synthetic Biology", "CRISPR Technology"]
     },
     {
       firstName: "Hauwa",
       lastName: "Garba",
       age: 20,
       score: 78,
-      courses: ["Geospatial AI", "Digital Archaeology"],
-      hobbies: "Painting, Virtual History Exploration"
+      courses: ["Geospatial AI", "Digital Archaeology"]
     },
     {
       firstName: "Tomnadoo",
       lastName: "Tekaa",
       age: 22,
       score: 92,
-      courses: ["Synthetic Media", "Extended Reality"],
-      hobbies: "Filmmaking, Virtual Art"
+      courses: ["Synthetic Media", "Extended Reality"]
     },
     {
       firstName: "Chidera",
       lastName: "Obinna",
       age: 18,
       score: 80,
-      courses: ["AI in Cybersecurity", "Neuromorphic Computing"],
-      hobbies: "Ethical hacking, Robotics"
+      courses: ["AI in Cybersecurity", "Neuromorphic Computing"]
     }
   ];
   
-  // Using .map() to create an array of description strings
+  // Using destructuring and .map() to create an array of strings description
   const studentDescriptions = students.map(
-    ({ firstName, lastName, age, score, courses, hobbies }) =>
-      `${firstName} ${lastName} is ${age} years old and scored ${score}. They are taking: ${courses.join(
-        ", "
-      )}. Hobbies: ${hobbies}.`
+    ({ firstName, lastName, age, score, courses }) =>
+      `${firstName} ${lastName} is ${age} years old and scored ${score}. He is taking: ${courses.join(", ")}`
   );
   
   console.log("Student Descriptions:");
   console.log(studentDescriptions);
   
-  // Using .forEach() to log each student's details.
+  // Using .forEach() to log each student's details
   console.log("\nIndividual Student Details:");
-  students.forEach(({ firstName, lastName, age, score, courses, hobbies }) => {
+  students.forEach(({ firstName, lastName, age, score, courses }) => {
     console.log(`Name: ${firstName} ${lastName}`);
     console.log(`Age: ${age}`);
     console.log(`Score: ${score}`);
     console.log(`Courses: ${courses.join(", ")}`);
-    console.log(`Hobbies: ${hobbies}`);
     console.log("-------------------------");
   });
